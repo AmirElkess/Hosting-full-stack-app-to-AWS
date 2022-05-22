1574d0f94af1ba5c321b569220c17026e49c44cb
AmirElkess <amir.elkess@gmail.com>
2022-05-22 21:31:23 +0200

    trial 3 (backend deploy)

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-emwftvcx.us-east-1.elasticbeanstalk.com:3000/api/v0",
+  apiHost: "http://udagram-api-dev.eba-vphvvvhb.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
